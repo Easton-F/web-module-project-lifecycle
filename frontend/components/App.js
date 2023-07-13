586df1b274b29a5654a44fd1ae675704e0a98a51
Easton <easton.fritsch21@gmail.com>
2023-07-13 12:21:33 -0600

    seperated Form component and imported it into App

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import Form from './Form'
 
 const URL = 'http://localhost:9000/api/todos'
 
@@ -70,11 +71,13 @@ export default class App extends React.Component {
               }, [])
             }
         </div>
-        <form id='todoForm' onSubmit={this.onTodoFormSubmit}>
-          <input value={this.state.todoNameInput} onChange={this.onInputChange} type='text' placeholder='Type todo'></input>
-          <input type='submit'></input>
-        </form>
-        <button onClick={this.toggleDisplayCompleted}>{this.state.displayCompleted ? 'Hide' : 'Show'} Completed</button>
+            <Form 
+              onTodoFormSubmit={this.onTodoFormSubmit}
+              todoNameInput={this.state.todoNameInput}
+              onInputChange={this.onInputChange}
+              toggleDisplayCompleted={this.toggleDisplayCompleted}
+              toggleCompleted={this.state.toggleCompleted}
+            />
       </div>
     )
   }
